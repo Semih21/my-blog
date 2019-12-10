e08f4e73ababf9910ce2dbe99b58c725a0917e7f
Semih217706 <semihkasimoglu@gmail.com>
2019-12-10 12:14:25 +0100

    first commit

---
+++ src/build/precache-manifest.d3da26d5eb8710798da6686c0ff44337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14a62700204914bedd1a301278406ed1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9c1884cdc0b43ab9e223",
+    "url": "/static/css/main.7b5f7dc8.chunk.css"
+  },
+  {
+    "revision": "c83255f67a884f9852d1",
+    "url": "/static/js/2.05018913.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.05018913.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c1884cdc0b43ab9e223",
+    "url": "/static/js/main.6c958cf9.chunk.js"
+  },
+  {
+    "revision": "2829fc8889d8b91e8c4b",
+    "url": "/static/js/runtime-main.b519a9b9.js"
+  }
+]);
